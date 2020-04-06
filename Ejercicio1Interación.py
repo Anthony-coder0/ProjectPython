5f322330a44ac1b5673f2928c5fa7fc5f6f4874d
Edwin Antonio Sión Tepé <63258728+Anthony-coder0@users.noreply.github.com>
2020-04-06 14:24:24 -0500

    Update Ejercicio1Interación.py

--- Ejercicio1Interación.py
+++ Ejercicio1Interación.py
@@ -1,6 +1,7 @@
-Ejercicio 1
-Hacer un script que le pida un numero al usuario 8 y que muestre la siguiente salida
-Ingrese un numero => 8
+#Ejercicio 1
+#Hacer un algoritmo en python 2 que le pida un numero al usuario 8 y que muestre la siguiente salida
+#Ingrese un numero => 8
+"""
 8
 9
 6
@@ -9,20 +10,20 @@ Ingrese un numero => 8
 3
 4
 5
+"""
 
 
 
 #!/usr/bin/python
-import time
 
-e = input("Ingrese un numero => ")
-t = 1
+e = input("Ingrese un numero => ") # Pedimos dato al usuario (entero) e => empezar
+i = 1 # Creamos una variable i que tendrá la inicialización 1
 
-for t in range(e):
-    print e
-    e += 1
-    print e
-    e  -= 3
+for i in range(e): # Le indicamos al for que con la variable (i) inicie el conteo en 1 y que entre el rango de la variable e
+    print e # Mostramos el dato que el usuario digito (8)
+    e += 1  # El primer caso nos dice mostrar el 9 lo cual sumamos 1 al valor de e (8)
+    print e # Mostramos el valor de la variable e
+    e  -= 3 # En cada iteración aumentamos y disminuimos el valor de e para determinar el valor que nos piden en el orden pedido
     print e
     e += 1
     print e
@@ -34,4 +35,4 @@ for t in range(e):
     print e
     e += 1
     print e
-    break
+    break  # Hacemos que las iteraciones de detengan
