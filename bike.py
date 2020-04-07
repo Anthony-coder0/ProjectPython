1091337f1d534042d0378be6e6e96c44ca6d5cfb
Edwin Antonio Sión Tepé <63258728+Anthony-coder0@users.noreply.github.com>
2020-04-06 20:46:29 -0500

    Update bike.py

--- bike.py
+++ bike.py
@@ -1,23 +1,23 @@
 # bike.py
-# let's define the class Bike
+# Definimos la Clase Bike
 class Bike:
-
-    def __init__(self, colour, frame_material):
+    # Cuerpo de la clase Bike
+    def __init__(self, colour, frame_material): # Método __init__ es el inicializador que configura los objetos con los valores color y material de marco 
         self.colour = colour
         self.frame_material = frame_material
 
     def brake(self):
         print("Braking!")
 
-# let's create a couple of instances
+# Instancias creadas con los atributos
 red_bike = Bike('Red', 'Carbon fiber')
 blue_bike = Bike('Blue', 'Steel')
 
-# let's inspect the objects we have, instances of the Bike class.
-print(red_bike.colour)  # prints: Red
-print(red_bike.frame_material)  # prints: Carbon fiber
-print(blue_bike.colour)  # prints: Blue
-print(blue_bike.frame_material)  # prints: Steel
+# inspeccionemos los objetos que tenemos, instancias de la clase Bike.
+print(red_bike.colour)  # Mostramos en pantalla: Rojo
+print(red_bike.frame_material)  # Mostramos en pantalla: Fibra de carbón
+print(blue_bike.colour)  # Mostramos en pantalla: Azul
+print(blue_bike.frame_material)  # Mostramos en pantalla: Acero
 
-# let's brake!
-red_bike.brake()  # prints: Braking!
+# Vamos a frenar :)
+red_bike.brake()  # Mostramos en pantalla: Frenado!
